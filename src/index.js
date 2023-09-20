5ed9c2e73d9c882802fc9e82086f4fa5a5007ed3
Vladimir <troyanskii1998@yandex.ru>
2023-09-20 20:18:02 +0300

    Добавляет загрузку данных о постах

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -10,10 +13,14 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
